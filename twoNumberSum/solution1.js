22e43f729355a686067c81a65ee38922a830777e
Alexander Selling <alexander@icontrolapp.se>
2020-09-17 09:31:26 +0200

    Formatting

--- twoNumberSum/solution1.js
+++ twoNumberSum/solution1.js
@@ -1,18 +1,18 @@
 function twoNumberSum(array, targetSum) {
-  for (var i=0;i<array.length;i++) {
-		var n1 = array[i];
-		for (var j=0;j<array.length;j++) {
-			if (i === j) {
-				continue;
-			}
-				
-			var n2 = array[j];
-			if (n1 + n2 === targetSum) {
-				return [n1, n2];
-			}
-		}
-	}
-	return []
+  for (var i = 0; i < array.length; i++) {
+    var n1 = array[i];
+    for (var j = 0; j < array.length; j++) {
+      if (i === j) {
+        continue;
+      }
+
+      var n2 = array[j];
+      if (n1 + n2 === targetSum) {
+        return [n1, n2];
+      }
+    }
+  }
+  return [];
 }
 
 // Do not edit the line below.
