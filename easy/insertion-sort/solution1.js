8034df6f263186399393002b03f97f16dee09315
zno <alexander@icontrolapp.se>
2021-08-26 15:43:28 +0200

    Update solution1.js

--- easy/insertion-sort/solution1.js
+++ easy/insertion-sort/solution1.js
@@ -1,37 +1,19 @@
 function insertionSort(array) {
-  for (var i = 1; i < array.length; i++) {
-    for (var j = i; j > 0; j--) {
-      var n1 = array[j];
-      var n2 = array[j - 1];
-
-      if (n1 < n2) {
-        swap(j, j - 1, array);
-      } else {
-        break;
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i];
+    let j = i - 1;
+    while (j >= 0) {
+      if (current < array[j]) {
+        let moveRight = array[j];
+        let moveLeft = array[j + 1];
+        array[j] = moveLeft;
+        array[j + 1] = moveRight;
       }
+      j--;
     }
   }
 
   return array;
-}
-
-function findTargetIndex(n1, array, cursor) {
-  var index = null;
-  for (var i = cursor; i >= 0; i--) {
-    var n2 = array[i];
-    if (n1 < n2) {
-      index = i;
-    }
-  }
-
-  return index;
-}
-
-function swap(i, j, array) {
-  var a = array[i];
-  var b = array[j];
-  array[j] = a;
-  array[i] = b;
 }
 
 // Do not edit the line below.
